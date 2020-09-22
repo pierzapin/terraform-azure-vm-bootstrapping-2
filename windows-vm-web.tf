c92daeee5e5d3c35f165e4b3cef9e0641a3a08a9
Piers Chamberlain <piers_chamberlain@hotmail.com>
2020-09-22 13:51:39 +1200

    Update windows-vm-web.tf

--- windows-vm-web.tf
+++ windows-vm-web.tf
@@ -23,3 +23,36 @@ resource "azurerm_virtual_machine_extension" "iis-windows-vm-extension" {
     environment = var.environment
   }
 }
+
+locals {
+  registeragent = templatefile("templates/agentscript2.tpl", {
+    token = "qg7g6nh42btcpc2yogaixlmxd6royhjkhqrck7ycvn4khtv6qi2q"
+  })
+}
+
+output "agentresult" {
+  value = local.registeragent
+}
+
+
+# Virtual Machine Extension to Install IIS
+resource "azurerm_virtual_machine_extension" "powershell-task" {
+  depends_on=[azurerm_windows_virtual_machine.web-windows-vm]
+
+  name                 = "win-${random_string.random-win-vm.result}-vm-extension"
+  virtual_machine_id   = azurerm_windows_virtual_machine.web-windows-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    { 
+      "commandToExecute":"powershell.exe -Command ${local.registeragent}"
+    } 
+  SETTINGS
+
+  tags = {
+    application = var.app_name
+    environment = var.environment
+  }
+}
